6b69a3894b9b8f3ba4be41690789a05a25b661e1
BArdelean <ardeleanflorinbogdan@gmail.com>
2020-08-17 17:58:40 +0300

    broke shit

--- public/video_overlay.js
+++ public/video_overlay.js
@@ -1,110 +1,155 @@
-jQuery(document).ready(function () {
-  const socketAddress = "ws://localhost:8080";
-  const ws = new WebSocket(socketAddress);
+var token = "";
+var uid = "";
 
-  ws.onmessage = function incoming(message) {
-    let payload = JSON.parse(message.data);
-    console.log(payload);
-    if (payload.stop_voting === true) {
-      $(".votes-container").html("");
+
+var twitch = window.Twitch.ext;
+
+var requests = {
+    set: createRequest('POST', '/control/send-vote')
+};
+
+function createRequest(type, method) {
+
+    return {
+        type: type,
+        url: "http://localhost:8080" + method,
+        contentType: 'application/json',
+        // dataType: "json",
+        // data: JSON.stringify({
+        //     vote: this.id
+        // })
     }
+}
+
+function setAuth(token) {
+    Object.keys(requests).forEach((req) => {
+        requests[req].headers = {
+            'Authorization': 'Bearer ' + token
+        }
+    });
+}
+twitch.onAuthorized(function(auth) {
+    token = auth.token;
+    uid = auth.userId;
+    channel = auth.channel_id
+    twitch.rig.log('setting auth')
+    twitch.rig.log(token, uid, channel)
+    
+    setAuth(token)
+    $.ajax(requests.set)
+})
+
+$(function() {
+    // listen for incoming broadcast message from our EBS
+    twitch.ext.listen('broadcast', function(target, contentType, payload) {
+        updateBlock(payload);
+        console.log("got it")
+    });
+});
+
+function updateBlock(payload) {
+
+    // let payload = JSON.parse(message.data);
+    console.log(payload)
+    if (payload.stop_voting === true) {
+        $(".votes-container").html("");
+    }
+
     //
     if (payload.position === "team_votes") {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=team-votes-container></div>");
-      delete payload.position;
-      for (item in payload) {
-        $(".team-votes-container").append(
-          `<button id = "${payload[item]}" class="button1"></button>`
-        );
-      }
+        $(".votes-container").html("");
+        $(".votes-container").append("<div class=team-votes-container></div>");
+        delete payload.position;
+        for (item in payload) {
+            $(".team-votes-container").append(
+                `<button id = "${payload[item]}" class="button1"></button>`
+            );
+        }
 
-      $("button").on("click", function () {
-        ws.send(this.id);
-        $(this).find(".button1").attr("disabled", "disabled");
-        $(".team-votes-container").html("");
-      });
-      //
+        $("button").on("click", function() {
+            $.ajax(requests.set)
+            $(this).find(".button1").attr("disabled", "disabled");
+            $(".team-votes-container").html("");
+        });
+
     } else if (payload.position === "left_horizontal") {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=left_horizontal></div>");
-
-      delete payload.position;
-      for (let item in payload) {
-        $(".left_horizontal").append(
-          `<button id = "${payload[item]}" class="button2"></button>`
-        );
-      }
-
-      $("button").on("click", function () {
-        ws.send(this.id);
-        $(this).find(".button2").attr("disabled", "disabled");
         $(".votes-container").html("");
-      });
-      //
+        $(".votes-container").append("<div class=left_horizontal></div>");
+
+        delete payload.position;
+        for (let item in payload) {
+            $(".left_horizontal").append(
+                `<button id = "${payload[item]}" class="button2"></button>`
+            );
+        }
+
+        $("button").on("click", function() {
+
+            $(this).find(".button2").attr("disabled", "disabled");
+            $(".votes-container").html("");
+        });
+        //
     } else if (payload.position === "left_vertical") {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=left_vertical></div>");
-      delete payload.position;
-
-      for (let item in payload) {
-        $(".left_vertical").append(
-          `<button id = "${payload[item]}" class="button3" ></button>`
-        );
-      }
-      $("button").on("click", function () {
-        ws.send(this.id);
-        $(this).find(".button3").attr("disabled", "disabled");
         $(".votes-container").html("");
-      });
-      //
+        $(".votes-container").append("<div class=left_vertical></div>");
+        delete payload.position;
+
+        for (let item in payload) {
+            $(".left_vertical").append(
+                `<button id = "${payload[item]}" class="button3" ></button>`
+            );
+        }
+        $("button").on("click", function() {
+
+            $(this).find(".button3").attr("disabled", "disabled");
+            $(".votes-container").html("");
+        });
+        //
     } else if (payload.position === "left_vertical_text") {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=left_vertical_text></div>");
-      delete payload.position;
-      for (let item in payload) {
-        $(".left_vertical_text").append(
-          `<button id = "${payload[item]}" class="button4"></button>`
-        );
-      }
-      $("button").one("click", function () {
-        ws.send(this.id);
-        $(":button").prop("disabled", true);
         $(".votes-container").html("");
-      });
-      //
-    } else if (payload.position === "right_horizontal") {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=right_horizontal></div>");
-      delete payload.position;
-      for (let item in payload) {
-        $(".right_horizontal").append(
-          `<button id = "${payload[item]}" class="button5"></button>`
-        );
-      }
-      $("button").on("click", function () {
-        ws.send(this.id);
-        $(this).find(".button5").attr("disabled", "disabled");
-        $(".votes-container").html("");
-      });
-    } else if (payload.hasOwnProperty("mvp_options")) {
-      $(".votes-container").html("");
-      $(".votes-container").append("<div class=player-votes-container></div>");
-      for (let item in payload.mvp_options) {
-        $(".player-votes-container").append(
-          `<button id = "${payload.mvp_options[item]}" class="button6" ></button>`
-        );
-      }
+        $(".votes-container").append("<div class=left_vertical_text></div>");
+        delete payload.position;
+        for (let item in payload) {
+            $(".left_vertical_text").append(
+                `<button id = "${payload[item]}" class="button4"></button>`
+            );
+        }
+        $("button").one("click", function() {
 
-      $("button").on("click", function () {
-        ws.send(this.id);
-        $(this).find(".button6").attr("disabled", "disabled");
-        $(".player-votes-container").html("");
-      });
+            $(":button").prop("disabled", true);
+            $(".votes-container").html("");
+        });
+        //
+    } else if (payload.position === "right_horizontal") {
+        $(".votes-container").html("");
+        $(".votes-container").append("<div class=right_horizontal></div>");
+        delete payload.position;
+        for (let item in payload) {
+            $(".right_horizontal").append(
+                `<button id = "${payload[item]}" class="button5"></button>`
+            );
+        }
+        $("button").on("click", function() {
+
+            $(this).find(".button5").attr("disabled", "disabled");
+            $(".votes-container").html("");
+        });
+    } else if (payload.hasOwnProperty("mvp_options")) {
+        $(".votes-container").html("");
+        $(".votes-container").append("<div class=player-votes-container></div>");
+        for (let item in payload.mvp_options) {
+            $(".player-votes-container").append(
+                `<button id = "${payload.mvp_options[item]}" class="button6" ></button>`
+            );
+        }
+
+        $("button").on("click", function() {
+            $(this).find(".button6").attr("disabled", "disabled");
+            $(".player-votes-container").html("");
+        });
     }
     setTimeout(() => {
-      $(this).find("button").attr("disabled", "disabled");
-      $(".votes-container").html("");
+        $(this).find("button").attr("disabled", "disabled");
+        $(".votes-container").html("");
     }, 60000);
-  };
-});
+}
--- services/server.js
+++ services/server.js
@@ -7,14 +7,23 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 const { json } = require("express");
+const jsonwebtoken = require('jsonwebtoken')
+const request = require('request')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 class Server {
   constructor(port = 8080) {
+    this.serverTokenDurationSec = 30
+    this.bearerPrefix = 'Bearer ';
+    this.secret = 'xPAxcyCsX8QkYHTo4HtGWJfy3MSkfBFD6l5DNfDVNjY='
+    this.clientId = 'uxmajdvtdy870ig106jnk04jg6e50y'
+    this.ownerID = '35667264'
     this.app = null;
     this.client = null;
     this.http = null;
     this.https = null;
     this.viewerVotes = [];
+    this.extSecret = Buffer.from(this.secret, 'base64')
     this.curentVotingSection = null;
     this.filteredVoteParams = null;
     this.setVotingParams = {
@@ -65,7 +74,7 @@ class Server {
         },
       },
     };
-
+    this.channel_id = '35667264'
     this.playerNames = null;
     this.createServers(port);
   }
@@ -77,10 +86,7 @@ class Server {
         type: "application/json",
       })
     );
-    var options = {
-      key: fs.readFileSync("client-key.pem"),
-      cert: fs.readFileSync("client-cert.pem"),
-    };
+
     this.app.use(cors());
     this.http = http.createServer(this.app);
     this.ws = new WebSocket.Server({ server: this.http });
@@ -98,26 +104,31 @@ class Server {
       "/control/set-player-names",
       this.onSetPlayerNames.bind(this)
     );
+    this.app.post('/control/send-vote', this.onIncomingVote.bind(this));
     this.app.get("/vote-results", this.onGetVoteResults.bind(this));
 
     this.http.listen(port, () => {
       console.log("Server started on port %d", this.http.address().port);
     });
+    console.log(this.secret)
+    console.log(this.extSecret)
   }
+  
 
   stop() {
     this.ws.close();
     this.http.close();
   }
+  onIncomingVote(req, res){
+    this.verifyAndDecode(req.headers.authorization);
+    let payload = JSON.parse(req.body)
+    this.viewerVotes.push(payload.vote);
+    res.sendStatus(200)
+  }
   onSetVoteParams(req, res) {
     let data = req.body;
     let voteParams = JSON.parse(data);
     this.setVotes = [];
-    // this.setVotingParams.viewer_interaction.custom_options =
-    //   voteParams.viewer_interaction.custom_options;
-    // this.filteredVoteParams = Object.entries(
-    //   voteParams.viewer_interaction.custom_options
-    // ).reduce((a, [k, v]) => (v ? ((a[k] = v), a) : a), {});
     if ("custom_options" in voteParams.viewer_interaction) {
       this.setVotingParams.viewer_interaction.custom_options =
         voteParams.viewer_interaction.custom_options;
@@ -126,10 +137,9 @@ class Server {
       ).reduce((a, [k, v]) => (v ? ((a[k] = v), a) : a), {});
       delete this.filteredVoteParams.question;
     }
-    console.log(this.filteredVoteParams);
-    console.log(voteParams);
     if (voteParams.viewer_interaction.start_custom === true) {
-      this.ws.broadcast(JSON.stringify(this.filteredVoteParams));
+      this.sendBroadcast(JSON.stringify(this.filteredVoteParams));
+      // this.ws.broadcast(JSON.stringify(this.filteredVoteParams));
       delete this.filteredVoteParams.position;
 
       for (let item in this.filteredVoteParams) {
@@ -137,6 +147,7 @@ class Server {
       }
       console.log(this.setVotes);
     } else if (voteParams.viewer_interaction.start_mvp === true) {
+      this.sendBroadcast(this.channel_id, JSON.stringify(this.playerNames));
       this.ws.broadcast(JSON.stringify(this.playerNames));
     } else if (voteParams.viewer_interaction.stop_voting === true) {
       this.ws.broadcast(JSON.stringify({ stop_voting: true }));
@@ -152,10 +163,55 @@ class Server {
   onSetPlayerNames(req, res) {
     let data = req.body;
     this.playerNames = JSON.parse(data);
-    console.log(this.playerNames);
     res.sendStatus(200);
   }
+   verifyAndDecode(header){
 
+    console.log(this.secret)
+    console.log(this.extSecret)
+    if (header.startsWith(this.bearerPrefix)) {
+      
+        let token = header.substring(this.bearerPrefix.length);
+        console.log(token)
+        return jsonwebtoken.verify(token, this.extSecret);
+    }}
+    makeServerToken() {
+  const payload = {
+    exp: Math.floor(Date.now() / 1000) + this.serverTokenDurationSec,
+    channel_id: this.channel_id,
+    user_id: this.ownerId, // extension owner ID for the call to Twitch PubSub
+    role: 'external',
+    pubsub_perms: {
+      send: ['*'],
+    },
+  };
+  console.log(jsonwebtoken.sign(payload, this.secret, { algorithm: 'HS256' }))
+  return jsonwebtoken.sign(payload, this.secret, { algorithm: 'HS256' });
+}
+sendBroadcast(payload){
+  const headers = {
+    'Client-ID': this.clientId,
+    'Content-Type': 'application/json',
+    'Authorization': this.bearerPrefix + this.makeServerToken(),
+  };
+  
+  
+  // Create the POST body for the Twitch API request.
+  const body = JSON.stringify({
+    content_type: 'application/json',
+    message: payload,
+    targets: ['broadcast'],
+  });
+console.log(body)
+  // Send the broadcast request to the Twitch API.
+  request(
+    `https://api.twitch.tv/extensions/message/${this.channel_id}`,
+    {
+      method: 'POST',
+      headers,
+      body,
+    })
+}
   calculatePercentages() {
     if (this.viewerVotes.length >= 1) {
       this.setVotes.forEach((element, index) => {
